768cc86944b4b39c25356d64a62d749dd5c5e71f
Benjamin Cane <ben@bencane.com>
2023-07-30 08:55:40 -0700

    Updating godoc details

--- is.go
+++ is.go
@@ -8,9 +8,12 @@ The design of this package is to return a regexp.Regexp object that can be
 used directly as if you defined the regular expression yourself.
 
 	// Validate an email address
-	isEmail := is.Email()
-	if isEmail.MatchString("email@example.com") {
+	if is.Email().MatchString("email@example.com") {
 	  fmt.Println("Valid email address")
 	}
+
+Regular expressions can be complex and challenging to create for every edge case.
+The goal of this project is to provide a collection that covers most common cases,
+while acknowledging that not all possible edge cases are addressed.
 */
 package is
